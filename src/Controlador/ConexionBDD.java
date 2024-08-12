66c927c48014536257e458aa51d115453c9981aa
kalexis69 <kevin.andaluz703@ist17dejulio.edu.ec>
2024-08-12 17:37:51 -0500

    Cambios de secretaria controlador y agregación de funciones al menú

--- src/Controlador/ConexionBDD.java
+++ src/Controlador/ConexionBDD.java
@@ -9,6 +9,7 @@ public class ConexionBDD {
 
     public Connection conectar() {
         try {
+<<<<<<< Updated upstream
             Class.forName("com.mysql.cj.jdbc.Driver");
             conexion = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/tur_resp_solicitudes_secretariag?autoReconnect=true&useSSL=false",
@@ -18,6 +19,16 @@ public class ConexionBDD {
             System.out.println("CONECTADO");
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("ERROR DE CONEXION A LA BASE DE DATOS: " + e);
+=======
+            //Manera de Conexión a la Base de Datos
+            Class.forName("com.mysql.jdbc.Driver");
+            //Parámetros de conexión url/usuario/clave en mysql
+            conexion=DriverManager.getConnection("jdbc:mysql://localhost/tur_resp_solicitudes_secretariag?autoReconnect=true&useSSL=false","root","0984315441k");
+            //System.out.println("CONECTADO"); 
+        } catch (ClassNotFoundException | SQLException e)//CAPTURAR ERRORES 
+        {
+             System.out.println("ERROR DE CONEXION A LA BASE DE DATOS"+ e);
+>>>>>>> Stashed changes
         }
         return conexion;
     }
